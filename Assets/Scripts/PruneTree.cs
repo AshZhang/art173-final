9e15fef36c3de8efcffdb14b0c7cfb665100a4a3
AshZhang <ashley.w.zhang@gmail.com>
2020-12-05 22:52:14 -0800

    Add candle, tree interactible

---
+++ Assets/Scripts/PruneTree.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruneTree : MonoBehaviour
+{
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = RoomControl.roomAnimator;
+        transform.Translate(Vector3.down * 0.45f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.childCount == 0){
+            anim.SetBool("transitioning", true);
+        }
+    }
+
+}
